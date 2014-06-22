35edc35984361e4cfd677822e553e077ac81b806
Rohit Verma <rohitverma@live.ca>
2014-06-22 00:46:51 -0400

    Adding google places search logic, posting book logic, updating map controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, $http, $rootScope) {
+    .controller('DashCtrl', function($scope, $http, $rootScope) {
         Parse.initialize("QChbWiKjeo1B17tFPlmZ6Xax7NcNRqY5BNM4urHa", "NXEGGmO3LlJTf64SYAa7QJ7gLytL5I5vAbxr5tur");
 
         var currentLat;
@@ -10,54 +10,71 @@ angular.module('starter.controllers', [])
                 currentLat = position.coords.latitude;
                 currentLong = position.coords.longitude;
 
+                var marker;
+                var myLatlng = new google.maps.LatLng(currentLat,currentLong);
+                var mapOptions = {
+                    zoom: 12,
+                    center: myLatlng
+                };
+                var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+//                var map = L.map('map', {
+//                    center: [currentLat, currentLong],
+//                    zoom: 14
+//                });
 
-                var map = L.map('map', {
-                    center: [currentLat, currentLong],
-                    zoom: 14
-                });
-
-                var latLngObject;
+                var latLngObject = {};
 
 
-                L.tileLayer('https://{s}.tiles.mapbox.com/v3/rohit-map.ideelj8n/{z}/{x}/{y}.png', {
-                    maxZoom: 18
-                }).addTo(map);
+//                L.tileLayer('https://{s}.tiles.mapbox.com/v3/rohit-map.ideelj8n/{z}/{x}/{y}.png', {
+//                    maxZoom: 18
+//                }).addTo(map);
 
                 var BookPosts = Parse.Object.extend($rootScope.category);
                 var bookPost = new Parse.Query(BookPosts);
+
                 bookPost.find({
                     success:function(posts){
                         console.log(posts);
                         for(var i = 0; i < posts.length; i++){
                             post = posts[i];
 
-                            if(post.attributes.location){
-                                $http({method: 'GET', url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+post.attributes.location+'&sensor=false'}).success(function(response){
+                            if(post.attributes.geoCode){
+                                console.log(post);
+                                latLngObject.lat = post.attributes.geoCode.k;
+                                latLngObject.lng = post.attributes.geoCode.A;
+                                console.log(latLngObject);
 
 
-                                    var infoWindowContent ='hi map point';
+                                    var infoWindowContent = post.attributes.location;
 
 
-                                    latLngObject = response.results[0].geometry.location;
 
-                                    console.log(response);
-                                    console.log("fuk yea", latLngObject);
-                                    if(latLngObject){
-                                        currentLat = latLngObject.lat;
-                                        currentLong = latLngObject.lng;
-                                    }
-                                    L.marker([currentLat, currentLong]).addTo(map)
-                                        .bindPopup(infoWindowContent).update();
-                                }).error(function(data){
-                                    console.log("error", data);
-                                });
+
+                                    marker = new google.maps.Marker({
+                                        position: latLngObject,
+                                        map: map,
+                                        title: 'Hello World!'
+                                    });
+                                closure(marker, infoWindowContent, i);
+
+
 
 
                             }
 
                         }
                     }
-                })
+                });
+
+                function closure(marker, infoWindowContent) {
+                    var infowindow = new google.maps.InfoWindow({
+                        content: infoWindowContent
+                    });
+
+                    google.maps.event.addListener(marker, 'click', function() {
+                        infowindow.open(marker.get('map'), marker);
+                    });
+                }
 
 
 
@@ -72,11 +89,11 @@ angular.module('starter.controllers', [])
         }
     })
 
-.controller('HomeCtrl', function($scope, $location, $rootScope) {
+    .controller('HomeCtrl', function($scope, $location, $rootScope) {
         $rootScope.user = {};
         Parse.initialize("QChbWiKjeo1B17tFPlmZ6Xax7NcNRqY5BNM4urHa", "NXEGGmO3LlJTf64SYAa7QJ7gLytL5I5vAbxr5tur");
         window.fbAsyncInit = function() {
-            FB.init({
+            Parse.FacebookUtils.init({
                 appId      : '252602678277336', // Facebook App ID
                 cookie     : true, // enable cookies to allow Parse to access the session
                 xfbml      : true  // parse XFBML
@@ -90,29 +107,40 @@ angular.module('starter.controllers', [])
             js.src = "//connect.facebook.net/en_US/all.js";
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
-        var currentUser = Parse.User.current();
+
         $scope.signIn = function(){
             console.log("hi");
 //            if(!currentUser) {
-                FB.Event.subscribe('auth.login', function () {
 
-                    $location.path('/tab/selection');
-                    $scope.$apply();
-                });
 //                Parse.FacebookUtils.logOut();
 
-
-            FB.login(function(response) {
-                if (response.authResponse) {
-                    $location.path('/tab/selection');
-                    $scope.$apply();
-
-
-
+            FB.getLoginStatus(function(response) {
+                if (response.status === 'connected') {
+                $location.path("/tab/selection");
+                $scope.$apply();
                 } else {
-                    console.log('User cancelled login or did not fully authorize.');
-                }
-            },{redirect_uri: "http://angelhack2014.pagekite.me/angelhack2014/www/#/tab/selection", display : 'touch'});
+
+                    Parse.FacebookUtils.logIn(null, {
+                        success: function(user){
+                            console.log(user);
+                            $location.path('/tab/selection');
+                            $scope.$apply();
+                        }
+                    }, {redirectUrl: "http://angelhack2014.pagekite.me/angelhack2014/www/#/tab/selection"});
+
+//                    FB.login(function (response) {
+//                        if (response.authResponse) {
+//
+//
+//
+//                            $location.path('/tab/selection');
+//                            $scope.$apply();
+//
+//
+//                        } else {
+//                            console.log('User cancelled login or did not fully authorize.');
+//                        }
+//                    }, {redirect_uri: "http://angelhack2014.pagekite.me/angelhack2014/www/#/tab/selection", display: 'touch'});
 //                    FB.login.logIn(null, {
 //                    success: function (user) {
 ////;                    window.location("http://angelhack2014.pagekite.me/angelhack2014/www/#/tab/selection");
@@ -125,22 +153,34 @@ angular.module('starter.controllers', [])
 //                });
 //            }
 //            else {
-//                $location.path("/tab/selection");
 //            }
+                }
+
+            });
+
         }
-})
+    })
 
     .controller('SelectionCtrl', function($scope, $rootScope, $location) {
         $scope.user = {};
         console.log("ehie");
         Parse.initialize("QChbWiKjeo1B17tFPlmZ6Xax7NcNRqY5BNM4urHa", "NXEGGmO3LlJTf64SYAa7QJ7gLytL5I5vAbxr5tur");
-        FB.api('/me', function (response) {
-            $scope.user.name = response.first_name;
 
-            $scope.$apply();
+        FB.getLoginStatus(function(response) {
+            if (response.status === 'connected') {
 
-        });
+                FB.api('/me', function (response) {
+                    $scope.user.name = response.first_name;
 
+                    $scope.$apply();
+
+                });
+            } else {
+                $scope.user.name = "Guest";
+            }
+
+
+    });
         $scope.setCategory = function(category){
             console.log(category);
             $rootScope.category = category;
@@ -151,28 +191,114 @@ angular.module('starter.controllers', [])
 
 
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
+    .controller('FriendsCtrl', function($scope, Friends) {
+//  $scope.friends = Friends.all();
+    })
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
-.controller('AccountCtrl', function($scope) {
-})
+    .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+//  $scope.friend = Friends.get($stateParams.friendId);
+    })
+    .controller('AccountCtrl', function($scope) {
+    })
 
-.controller('ListingsCtrl', function($scope){
-	Parse.initialize("QChbWiKjeo1B17tFPlmZ6Xax7NcNRqY5BNM4urHa", "NXEGGmO3LlJTf64SYAa7QJ7gLytL5I5vAbxr5tur");
-	var ListItem= Parse.Object.extend("BookPost");
-	var query= new Parse.Query(BookPost);
-	query.find({
-		success: function(listItem){
-			console.dir(listItem);
-		},
-		error: function(listItem, error){
-			console.log("error" + error);
-		}
-	});
-});
+    .controller('PostListingCtrl', function($scope){
+        var user = Parse.User.current();
+        var location;
+        var geoCode;
+        var BookPost = Parse.Object.extend("BookPost");
+        var bookPost = new BookPost();
+        autocomplete = new google.maps.places.Autocomplete(
+            /** @type {HTMLInputElement} */(document.getElementById('autoComplete')));
+        // When the user selects an address from the dropdown,
+        // populate the address fields in the form.
+        google.maps.event.addListener(autocomplete, 'place_changed', function() {
+            console.log(autocomplete.getPlace());
+            geoCode = autocomplete.getPlace().geometry.location;
+            location = autocomplete.getPlace().formatted_address;
+            console.log(geoCode);
+
+            bookPost.set({
+                location: location,
+                geoCode: geoCode,
+                postCondition: "Good",
+                postISBN: "22",
+                postName: "e",
+                postTitle: "Book"
+            });
+
+            bookPost.save(null, {
+                success: function(gameScore) {
+                    // Execute any logic that should take place after the object is saved.
+                    console.log('New object created with objectId: ' + gameScore.id);
+                },
+                error: function(gameScore, error) {
+                    // Execute any logic that should take place if the save fails.
+                    // error is a Parse.Error with an error code and description.
+                    console.log('Failed to create new object, with error code: ' + error.description);
+                }
+            });
+
+        });
+
+
+
+
+
+    })
+    .controller('NavigationCtrl', function($scope) {
+        // Create the autocomplete object, restricting the search
+        // to geographical location types.
+        var user = Parse.User.current();
+        var location;
+        var geoCode;
+        var BookPost = Parse.Object.extend("BookPost");
+        var bookPost = new BookPost();
+        autocomplete = new google.maps.places.Autocomplete(
+            /** @type {HTMLInputElement} */(document.getElementById('autoComplete')));
+        // When the user selects an address from the dropdown,
+        // populate the address fields in the form.
+        google.maps.event.addListener(autocomplete, 'place_changed', function() {
+            console.log(autocomplete.getPlace());
+            geoCode = autocomplete.getPlace().geometry.location;
+            location = autocomplete.getPlace().formatted_address;
+            console.log(geoCode);
+
+            bookPost.set({
+                location: location,
+                geoCode: geoCode,
+                postCondition: "Good",
+                postISBN: "22",
+                postName: "e",
+                postTitle: "Book"
+            });
+
+            bookPost.save(null, {
+                success: function(gameScore) {
+                    // Execute any logic that should take place after the object is saved.
+                    console.log('New object created with objectId: ' + gameScore.id);
+                },
+                error: function(gameScore, error) {
+                    // Execute any logic that should take place if the save fails.
+                    // error is a Parse.Error with an error code and description.
+                    console.log('Failed to create new object, with error code: ' + error.description);
+                }
+            });
+
+        });
+
+    })
+    .controller('ListingsCtrl', function($scope){
+//	Parse.initialize("QChbWiKjeo1B17tFPlmZ6Xax7NcNRqY5BNM4urHa", "NXEGGmO3LlJTf64SYAa7QJ7gLytL5I5vAbxr5tur");
+//	var ListItem= Parse.Object.extend("BookPost");
+//	var query= new Parse.Query(BookPost);
+//	query.find({
+//		success: function(listItem){
+//			console.dir(listItem);
+//		},
+//		error: function(listItem, error){
+//			console.log("error" + error);
+//		}
+//	});
+    });
 
 
